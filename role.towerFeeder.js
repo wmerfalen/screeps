1487353dbf13dceb6bc9e15a964a0faee0a9ecff
Will Merfalen <wmerfalen@xennsoft.com>
2017-07-03 18:50:22 -0600

    towerfeeder

--- role.towerFeeder.js
+++ role.towerFeeder.js
@@ -6,74 +6,120 @@
  * var mod = require('role.harvester');
  * mod.thing == 'a thing'; // true
  */
-var config = require('config');
-var scale = require('scale');
-var roleTower = require('role.tower');
-var parent = require('role.parent');
-var creepFunc = require('functions.creep');
+var config = require('config.js');
+var scale = require('scale.js');
+var ext = require('struct.extension.js');
+var parent = require('role.parent.js');
+var creepFunc = require('functions.creep.js');
+var events = require('events.js');
+var roleTowerFeeder = function(){
+    parent.call(this);
+}
 
-var roleTowerFeeder = function(){};
 roleTowerFeeder.prototype = Object.create(parent.prototype);
 
-
+roleTowerFeeder.prototype.constructor = roleTowerFeeder;
 roleTowerFeeder.prototype.run = function(creep) {
-    if(creep.carry.energy < creep.carryCapacity) {
-        var sources = creep.room.find(FIND_SOURCES);
-        if(creep.harvest(sources[config.harvesterSource(creep)]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[config.harvesterSource(creep)]);
+    
+        //TODO: finish this code to make it work for tower feeders
+        console.log('tower feeder stub');
+        return;
+        var spawn = config.spawn();
+        if(spawn.energy < spawn.energyCapacity){
+                creep.memory.energy_fallback = false;
         }
-    }
-    else {
-        var tower = new roleTower();
-        var towers = tower.getAllTowers(config.room());
-        var target = null;
-        for(var i in towers){
-            //TODO: Which tower is lowest? Go to that one as a priority
-            if(towers[i].energy < towers[i].energyCapacity){
-                target = towers[i];
-                break;
+        if(typeof spawn == "undefined"){
+            console.log("SPAWN undefined");
+        }
+        
+        if(creep.carry.energy == 0){
+            creep.memory.energy_full = false;
+        }
+        if(creep.carry.energy < creep.carryCapacity && !creep.memory.energy_full){
+            var sources = creep.room.find(FIND_SOURCES);
+            var ret = creep.harvest(sources[config.harvesterSource(creep)]);
+            if( ret == ERR_NOT_IN_RANGE) {
+                console.log("harvester - moving to source");
+                creep.moveTo(sources[config.harvesterSource(creep)]);
+            }else{
+                switch(ret){
+                    case ERR_INVALID_TARGET:
+                        console.log("Invalid source passed to creep[harvester]... searching..");
+                        
+                }
+                console.log("Harvest unhandled return: " + ret);
+            }
+            return;
+        }
+	    if(creep.carry.energy < creep.carryCapacity && creep.carry.energy) {
+	        console.log("harvester carry energy < carry capacity");
+	        //If we were in the middle of a transfer, attempt to offload to another extension
+	        if(creep.memory.energy_full){
+	            //Move to the next extension
+	            var nextExt = ext.nextEnergyQueue(creep);
+	            if(nextExt !== null){
+	                creep.memory.transfering = true;
+	                var r= creep.transfer(nextExt,RESOURCE_ENERGY);
+	                if(r == ERR_NOT_IN_RANGE){
+	                    creep.moveTo(nextExt);
+	                    return;
+	                }
+	                if(r == ERR_FULL){
+	                    console.log("harvester recursion");
+	                    this.run(creep);
+	                    return;
+	                }
+	            }
+	        }
+        }else {
+            if(creep.memory.energy_fallback !== false){
+                var energyFallback = ext.nextEnergyQueue(creep);
+                var r = creep.transfer(energyFallback,RESOURCE_ENERGY);
+                if(r == ERR_NOT_IN_RANGE){
+                    console.log("harvester moving to energy fallback");
+                    creep.moveTo(energyFallback);
+                    return;
+                }
+                
+            }
+            var transferReturn = 0;
+            console.log("harvester transfering to spawn");
+            switch(transferReturn = creep.transfer(spawn,RESOURCE_ENERGY)){
+                case ERR_FULL:
+                    console.log("harvester - Spawn full");
+                    creep.memory.energy_fallback = true;
+                    creep.memory.energy_full = true;
+                    return {'trigger_type':'spawn_full','trigger_unless': 1,'trigger_unless_cb': function(events_object){
+                            if(events_object.has_seen('spawn_not_full')){
+                                return false;
+                            }else{
+                                return true;
+                            }
+                        }
+                    };
+                    break;
+                case ERR_NOT_IN_RANGE:
+                    console.log("harvester - moving to spawn");
+                    creep.moveTo(spawn);
+                    break;
+                default:
+                    console.log("Unhandled creep.transfer:" + transferReturn);
+                    break;
             }
         }
-
-        if(target === null){
-            creep.memory.role = 'upgrader';
-            return;
-        }
-        
-        var r = creep.transfer(target,RESOURCE_ENERGY);
-        if(r == ERR_NOT_IN_RANGE){
-            creep.moveTo(target);
-            return;
-        }
-        
-        if(r != 0){
-            console.log('towerFeeder: unhandled return type:' + r);
-        }
-    }
+	};
+roleTowerFeeder.prototype.maxCreep = function(){	
+    //TODO: dynamically calculate the number of required harvesters
+    return 4;
 };
-
-roleTowerFeeder.prototype.maxCreep = function(){
-    return 2;
-}
-
 roleTowerFeeder.prototype.getSpawnWeight = function(){
-    var tower = new roleTower();
-    var towers = tower.getAllTowers(config.room());
-    console.log(towers);
     var spawnWeight = 0;
-    var creepCount = creepFunc.getCreepCount('towerFeeder');
-    if(towers.length == 0 || creepCount >= this.maxCreep()){
-        return 0;
+    
+    if(creepFunc.getCreepCount('harvester') < 2){
+        spawnWeight += 6;
     }
-    for(var i in towers){
-        if(towers[i].energy == 0){
-            spawnWeight += 2;
-            if(creepCount == 0){
-                spawnWeight += 2;
-            }
-        }
-    }
+
     return spawnWeight;
-}
+}	
 
 module.exports = roleTowerFeeder;
